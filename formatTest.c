987e3ebfa35ee808c686eb052b82c7187afe8363
amckoy42 <amckoy000@citymail.cuny.edu>
2021-05-27 00:35:28 -0400

    Add part 2

--- formatTest.c
+++ formatTest.c
@@ -3,8 +3,12 @@
 int main(){
     const int n = 128;
     char buffer[n];
+    int target = 4660; //0x1234
     fgets(buffer, n, stdin);
     printf(buffer);
+    if(target != 4660){
+        printf("target overwritten\n");
+    }
 
     return 0;
 }
